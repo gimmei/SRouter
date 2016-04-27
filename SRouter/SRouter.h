7a598de29086a66b6acaaab086e01aa0d967b79d
cs <angelcs1990@sohu.com>
2016-04-27 10:04:04 +0800

    podspec and srouter.h change

--- SRouter/SRouter.h
+++ SRouter/SRouter.h
@@ -9,12 +9,20 @@
 #ifndef SRouter_h
 #define SRouter_h
 
+#if __has_include(<SRouter/SRouter.h>)
 
+#import <publicInf_cmds.h>
+#import <publicInf_macro.h>
+#import <RouterHandlerInf.h>
+#import <ModuleProtocol.h>
+#import <SRouterRemote.h>
+#import <SRouterLocal.h>
+#else
 #import "publicInf_cmds.h"
 #import "publicInf_macro.h"
 #import "RouterHandlerInf.h"
 #import "ModuleProtocol.h"
 #import "SRouterRemote.h"
 #import "SRouterLocal.h"
-
+#endif
 #endif /* SRouter_h */
